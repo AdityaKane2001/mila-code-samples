e085c65f9863f7059407f517a60a7ae7c1d73e05
Aditya Kane <64411306+AdityaKane2001@users.noreply.github.com>
2022-12-01 20:21:14 +0530

    Update regnet.py

--- regnets/regnet.py
+++ regnets/regnet.py
@@ -1,3 +1,5 @@
+# Credits: https://github.com/keras-team/keras/blob/master/keras/applications/regnet.py
+
 # Copyright 2021 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
